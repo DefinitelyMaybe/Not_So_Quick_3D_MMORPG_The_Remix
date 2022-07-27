3b4627b9c035b38949553a4e8bb4ea204276fdf8
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-07-28 10:40:46 +1200

    it was that easy to make an entity viewer

--- src/routes/entityViewer.svelte
+++ src/routes/entityViewer.svelte
@@ -1,62 +1,59 @@
 <script>
-	// import { onMount } from 'svelte';
-	// import { World } from '../worlds/entityViewer.js';
-	import * as THREE from 'three';
-	import { Canvas, AmbientLight, Mesh, OrbitControls, PerspectiveCamera } from '@threlte/core';
+	import { CircleBufferGeometry, MeshStandardMaterial, BoxBufferGeometry, DoubleSide } from 'three'
+  import { DEG2RAD } from 'three/src/math/MathUtils'
+  import {
+    AmbientLight,
+    Canvas,
+    DirectionalLight,
+    Group,
+    HemisphereLight,
+    Mesh,
+    OrbitControls,
+    PerspectiveCamera
+  } from '@threlte/core'
+	import { GLTF } from "@threlte/extras";
+  import { spring } from 'svelte/motion'
+	import { modelData } from "../data/models/mod.js";
 
-	// import Menu from '$lib/game/menu.svelte';
-	// import ModelPicker from '$lib/dev/models.svelte';
-	// import Inspector from '$lib/dev/inspector.svelte';
+	let modelURL = modelData.paladin.url
 
-	// let world;
-	// let model = 'paladin';
-	// let obj;
-	let scale = 1;
-
-	// let focused = false;
-
-	// function test() {
-	// 	console.log(world);
-	// }
-
-	// function test2() {
-	// 	console.log(world.camera);
-	// 	console.log(world.entity.camera);
-	// }
-
-	// onMount(() => {
-	// 	world = new World();
-	// 	world.resize();
-	// 	obj = world.entities.get(0);
-	// });
+  const scale = spring(1)
 </script>
 
-<Canvas>
-	<PerspectiveCamera fov={90}>
-		<OrbitControls enableDamping />
-	</PerspectiveCamera>
+<div class="relative h-full w-full">
+  <Canvas>
+    <PerspectiveCamera position={{ x: 10, y: 10, z: 10 }} fov={24}>
+      <OrbitControls
+        maxPolarAngle={DEG2RAD * 80}
+        target={{ y: 0.5 }}
+      />
+    </PerspectiveCamera>
 
-	<AmbientLight intensity={0.2} />
+    <DirectionalLight shadow position={{ x: 3, y: 10, z: 10 }} />
+    <DirectionalLight position={{ x: -3, y: 10, z: -10 }} intensity={0.2} />
+    <AmbientLight intensity={0.2} />
 
-	<!-- Cube -->
-	<Mesh
-		interactive
-		on:pointerenter={() => (scale = 2)}
-		on:pointerleave={() => (scale = 1)}
-		position={{ y: 0.5 }}
-		castShadow
-		geometry={new THREE.BoxBufferGeometry(1, 1, 1)}
-		material={new THREE.MeshStandardMaterial({ color: '#333333' })}
-	/>
+    <!-- Cube -->
+    <Group scale={$scale}>
+			<GLTF url={modelURL} />
+    </Group>
 
-	<!-- Floor -->
-	<Mesh
-		receiveShadow
-		rotation={{ x: -90 * (Math.PI / 180) }}
-		geometry={new THREE.CircleBufferGeometry(3, 72)}
-		material={new THREE.MeshStandardMaterial({ side: THREE.DoubleSide, color: 'white' })}
-	/>
-</Canvas>
+    <!-- Floor -->
+    <Mesh
+      receiveShadow
+      rotation={{ x: -90 * (Math.PI / 180) }}
+      geometry={new CircleBufferGeometry(3, 72)}
+      material={new MeshStandardMaterial({ side: DoubleSide, color: '#333333' })}
+    />
+  </Canvas>
+	<div class="absolute top-0 h-full w-full pointer-events-none">
+		<div class="flex flex-col items-center h-full">
+			<form class="pointer-events-auto">
+				<button class="btn">hello world</button>
+			</form>
+		</div>
+	</div>
+</div>
 <!-- <canvas class="absolute" id="game" />
 <div class="absolute z-10">
 	<Menu on:test={test} on:test2={test2} />
