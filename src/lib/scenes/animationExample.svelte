abcb7ce66a0de0900eff0914e7e561224b1a9157
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-07-29 16:44:10 +1200

    trying to flesh out a scene

--- src/lib/scenes/animationExample.svelte
+++ src/lib/scenes/animationExample.svelte
@@ -1,7 +1,24 @@
 <script>
-  import { useGltfAnimations } from "@threlte/extras";
-  import { modelData } from "../../data/models/mod.js";
+	import * as THREE from 'three';
+	import { DEG2RAD } from 'three/src/math/MathUtils';
+	import { PerspectiveCamera, DirectionalLight, AmbientLight, Mesh } from '@threlte/core';
+	import { useGltfAnimations, GLTF } from '@threlte/extras';
+	import { modelData } from '../../data/models/mod.js';
 
-  console.log(modelData);
-  
+	const { name, url } = modelData.sorceror;
+	// const { gltf, actions } = useGltfAnimations<'All Animations'>(({ actions }) => {
+	//   // Either play your animations as soon as they are loaded
+	//   actions['All Animations']?.play()
+	// })
 </script>
+
+<PerspectiveCamera fov={90} />
+
+<DirectionalLight position={{ x: -3, y: 10, z: -10 }} intensity={0.2} />
+<AmbientLight intensity={0.5} />
+
+<GLTF {url} />
+<Mesh
+	material={new THREE.MeshStandardMaterial()}
+	geometry={new THREE.CircleBufferGeometry()}
+	rotation={{ x: DEG2RAD * -90 }} />
