cbb72cabe110e490c6953a0b38cad6c66cf1d577
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-08-16 15:25:14 +1200

    animations in threlte might be off

--- src/lib/scenes/animationExample.svelte
+++ src/lib/scenes/animationExample.svelte
@@ -1,41 +1,135 @@
 <script>
 	import * as THREE from 'three';
+	import { onDestroy, onMount } from 'svelte';
 	import { DEG2RAD } from 'three/src/math/MathUtils';
-	import { PerspectiveCamera, DirectionalLight, AmbientLight, Mesh, OrbitControls } from '@threlte/core';
+	import {
+		PerspectiveCamera,
+		DirectionalLight,
+		AmbientLight,
+		Mesh,
+		OrbitControls
+	} from '@threlte/core';
 	import { useGltfAnimations, GLTF, HTML } from '@threlte/extras';
 	import { modelData } from '../../data/models/mod.js';
+	import { state } from './animationExample.js';
 
 	const { name, url } = modelData.sorceror;
-	const { gltf, actions, mixer } = useGltfAnimations(setup)
-
-	const triggerAnimation = () => {
-    if ($mixer) $mixer.timeScale = 0.5
-    $actions['Attack']?.play()
-  }
-	
-	function setup({ actions }) {
+	const { gltf, actions, mixer } = useGltfAnimations(({ actions }) => {
 		console.log(actions);
-	  // Either play your animations as soon as they are loaded
-	  actions['Idle']?.play()
+		// Armature|mixamo.com|Layer0
+		// Attack,
+		// Dance,
+		// Death,
+		// Idle,
+		// Run,
+		// Walk
+		// Either play your animations as soon as they are loaded
+		actions['Idle']?.play();
+	});
+	let init = true;
+	let cam
+
+	const unsubscribe = state.subscribe(() => {
+		// triggerAnimation();
+		if (init) return;
+		console.log('hello');
+		prepareCrossFade('Idle', 'Run', 0.5);
+	});
+
+	// const triggerAnimation = () => {
+	// 	if (!$gltf) return;
+
+	// 	$actions['Idle']?.stop();
+	// 	$actions['Attack']?.play();
+	// 	const onLoopFinished = () => {
+	// 		$mixer.removeEventListener('loop', onLoopFinished);
+	// 		console.log('loop');
+	// 		$actions['Attack']?.stop();
+	// 		$actions['Idle']?.play();
+	// 	};
+	// 	$mixer.addEventListener('loop', onLoopFinished);
+	// };
+
+	function prepareCrossFade(startAction, endAction, defaultDuration) {
+		// Switch default / custom crossfade duration (according to the user's choice)
+		const duration = defaultDuration;
+
+		// Make sure that we don't go on in singleStepMode, and that all actions are unpaused
+		// singleStepMode = false;
+		unPauseAllActions();
+
+		// If the current action is 'idle' (duration 4 sec), execute the crossfade immediately;
+		// else wait until the current action has finished its current loop
+		if (startAction === 'Idle') {
+			executeCrossFade(startAction, endAction, duration);
+		} else {
+			synchronizeCrossFade(startAction, endAction, duration);
+		}
 	}
+
+	function synchronizeCrossFade(startAction, endAction, duration) {
+		$mixer.addEventListener('loop', onLoopFinished);
+
+		function onLoopFinished(event) {
+			if (event.action === startAction) {
+				$mixer.removeEventListener('loop', onLoopFinished);
+
+				executeCrossFade(startAction, endAction, duration);
+			}
+		}
+	}
+
+	function executeCrossFade(startAction, endAction, duration) {
+		// Not only the start action, but also the end action must get a weight of 1 before fading
+		// (concerning the start action this is already guaranteed in this place)
+
+		setWeight(endAction, 1);
+		$actions[endAction].time = 0;
+
+		// Crossfade with warping - you can also try without warping by setting the third parameter to false
+
+		$actions[startAction].crossFadeTo($actions[endAction], duration, true);
+	}
+
+	// This function is needed, since animationAction.crossFadeTo() disables its start action and sets
+	// the start action's timeScale to ((start animation's duration) / (end animation's duration))
+
+	function setWeight(action, weight) {
+		$actions[action].enabled = true;
+		$actions[action].setEffectiveTimeScale(1);
+		$actions[action].setEffectiveWeight(weight);
+	}
+
+	function unPauseAllActions() {
+		for (const action in $actions) {
+			if ($actions[action].paused) {
+				console.log(action, 'was paused');
+			}
+			$actions[action].paused = false;
+		}
+	}
+
+	onMount(() => {
+		init = false;
+	});
+	onDestroy(unsubscribe);
 </script>
 
-<PerspectiveCamera position={{ x: 3, y: 3, z: 3 }} fov={90}>
-	<OrbitControls
-		maxPolarAngle={DEG2RAD * 80}
-		autoRotate={false}
-		enableZoom={false}
-		target={{ y: 0.5 }}
-	/>
+<PerspectiveCamera bind:camera={cam} position={{ x: -0.9, y: 1.6, z: 1.5 }} fov={90}>
+	<OrbitControls  target={{ y: 1.2 }} />
 </PerspectiveCamera>
+<!-- maxPolarAngle={DEG2RAD * 80} -->
 
 <DirectionalLight shadow position={{ x: 3, y: 10, z: 10 }} />
-    <DirectionalLight position={{ x: -3, y: 10, z: -10 }} intensity={0.2} />
-    <AmbientLight intensity={0.2} />
+<DirectionalLight position={{ x: -3, y: 10, z: -10 }} intensity={0.2} />
+<AmbientLight intensity={0.2} />
 
+<HTML center transform distanceFactor={1} position={{y:2.2}} lookAt={cam} occlude>
+	<button class="btn">{name}</button>
+</HTML>
 <GLTF {url} bind:gltf={$gltf}/>
 
 <Mesh
-	material={new THREE.MeshStandardMaterial()}
+	material={new THREE.MeshStandardMaterial({side: THREE.DoubleSide})}
 	geometry={new THREE.CircleBufferGeometry(3, 72)}
 	rotation={{ x: DEG2RAD * -90 }} />
--- src/routes/animation.svelte
+++ src/routes/animation.svelte
@@ -1,16 +1,28 @@
 <script>
-	import { Canvas } from "@threlte/core";
-	import Scene from "$lib/scenes/animationExample.svelte";
+	import { Canvas } from '@threlte/core';
+	import Scene from '$lib/scenes/animationExample.svelte';
+	import { state } from '$lib/scenes/animationExample.js';
 </script>
 
 <div class="relative h-full w-full">
-  <Canvas>
-    <Scene/>
-  </Canvas>
+	<Canvas>
+		<Scene />
+	</Canvas>
 	<div class="absolute top-0 h-full w-full pointer-events-none">
 		<div class="flex flex-col items-center h-full">
 			<div class="pointer-events-auto">
 				<a class="btn m-2" href="/">return</a>
+			</div>
+		</div>
+	</div>
+	<div class="absolute top-0 h-full w-full pointer-events-none">
+		<div class="flex items-center h-full">
+			<div class="pointer-events-auto">
+				<button
+					class="btn m-2"
+					on:click={() => {
+						$state = !$state;
+					}}>Test</button>
 			</div>
 		</div>
 	</div>
