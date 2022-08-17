e4d003a0159f781db18d0628ab5ae73ab5c630d6
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-08-17 15:35:07 +1200

    add prefetch and terrain route

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,10 +2,11 @@
 	<title>Hello Dev</title>
 </svelte:head>
 
-<main class="flex flex-col h-full bg-gray-600 justify-center items-center">
+<main class="flex flex-col h-full justify-center items-center">
 	<!-- <a class="btn m-1" sveltekit:prefetch href="login">Login</a> -->
-	<a class="btn m-1" href="/threlteDoor">Threlte Door</a>
-	<a class="btn m-1" href="/entityViewer">Entity Viewer</a>
-	<a class="btn m-1" href="/repl">REPL</a>
-	<a class="btn m-1" href="/animation">animation</a>
+	<a class="btn m-1" sveltekit:prefetch href="/threlteDoor">Threlte Door</a>
+	<a class="btn m-1" sveltekit:prefetch href="/entityViewer">Entity Viewer</a>
+	<a class="btn m-1" sveltekit:prefetch href="/repl">REPL</a>
+	<a class="btn m-1" sveltekit:prefetch href="/animation">animation</a>
+	<a class="btn m-1" sveltekit:prefetch href="/terrain">terrain</a>
 </main>
