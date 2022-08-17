7bc9b5c6fcc432db708bfc90f7cc17ff1e83d49d
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-08-18 10:57:52 +1200

    remove unused imports

--- src/lib/scenes/terrainExample.svelte
+++ src/lib/scenes/terrainExample.svelte
@@ -1,22 +1,15 @@
 <script lang="ts">
-	import {
-		DirectionalLight,
-		PerspectiveCamera,
-		OrbitControls
-	} from '@threlte/core'
-	import { CollisionGroups, Debug } from '@threlte/rapier'
-  import Terrain from '../Terrain.svelte'
-	import Door from '../Door.svelte'
-  import Player from "../Player.svelte";
+	import { DirectionalLight, PerspectiveCamera, OrbitControls } from '@threlte/core';
+	import { CollisionGroups, Debug } from '@threlte/rapier';
+	import Terrain from '../Terrain.svelte';
 
-  let mesh
-  let cam
+	let cam;
 
-  export const gui = {
-    getCamera(){
-      return cam
-    }
-  }
+	export const gui = {
+		getCamera() {
+			return cam;
+		}
+	};
 </script>
 
 <Debug />
@@ -26,20 +19,6 @@
 </PerspectiveCamera>
 <DirectionalLight shadow position={{ y: 20, x: 8, z: -3 }} />
 
-<!--
-	The ground needs to be on both group 15 which is the group
-	to detect the groundedness of the player as well as on group
-	0 which is the group that the player is actually physically
-	interacting with.
- -->
 <CollisionGroups groups={[0, 15]}>
 	<Terrain />
 </CollisionGroups>
-
-<!--
-	All physically interactive stuff should be on group 0
--->
-<!-- <CollisionGroups groups={[0]}>
-	<Player bind:playerMesh={mesh} position={{ z: -3, y: 2 }} />
-	<Door />
-</CollisionGroups> -->
