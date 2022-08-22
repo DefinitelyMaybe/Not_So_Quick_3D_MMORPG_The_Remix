665a5f1fd5043a853f811eaf608c2e57ec732882
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-08-22 13:44:00 +1200

    closer to desired controller

--- src/lib/characters/PlayerV2.svelte
+++ src/lib/characters/PlayerV2.svelte
@@ -1,15 +1,13 @@
 <script>
-	import * as THREE from 'three';
-	import { PerspectiveCamera, OrbitControls } from '@threlte/core';
 	import { GLTF, useGltfAnimations, HTML } from '@threlte/extras';
 	import { modelData } from '../../data/models/mod.js';
-	import PlayerController from '../controllers/PlayerController.svelte';
-	import { DEG2RAD } from 'three/src/math/MathUtils.js';
+	import PlayerController from '../controllers/FollowObjectController.svelte';
 
 	export let position = undefined;
 	export let cam;
-	let camRotation = [0, 0, 0];
+
 	let scene;
+	let camRotation
 
 	const { name, url } = modelData.sorceror;
 	const { gltf, actions } = useGltfAnimations(({ actions }) => {
@@ -22,6 +20,10 @@
 	let backward = false;
 	let left = false;
 	let right = false;
+
+	$: if (camRotation && scene) {
+		scene.rotation.set(camRotation[0], camRotation[1], camRotation[2]);
+	}
 
 	function transitionTo(newAction, duration = 1) {
 		const action = $actions[currentAction];
@@ -112,19 +114,6 @@
 
 <svelte:window on:keydown={handleKeyDown} on:keyup={handleKeyUp} />
 
-<PerspectiveCamera bind:camera={cam} position={{ x: 6, y: 14, z: 5 }}>
-	<OrbitControls
-		target={position}
-		on:change={() => {
-			if (!scene) return;
-			const camEuler = cam.rotation.clone();
-			camEuler.reorder('YXZ');
-			camEuler.x = 0;
-			scene.rotation.set(camEuler.x, camEuler.y, camEuler.z);
-			camRotation = [...camEuler];
-		}} />
-</PerspectiveCamera>
-
 <!-- To detect the groundedness of the player, a collider on group 15 is used -->
 <PlayerController
 	{position}
@@ -133,7 +122,9 @@
 	height={1.8}
 	jumpStrength={2}
 	playerCollisionGroups={[0]}
-	groundCollisionGroups={[15]}>
+	groundCollisionGroups={[15]}
+	bind:cam
+	bind:camRotation>
 	<GLTF {url} receiveShadow castShadow bind:scene bind:gltf={$gltf}>
 		<HTML center transform distanceFactor={2} position={{ y: 2.2 }} lookAt={cam}>
 			<button
@@ -144,128 +135,3 @@
 		</HTML>
 	</GLTF>
 </PlayerController>
-
-<!-- if (params.firstPerson) {
-	controls.maxPolarAngle = Math.PI;
-	controls.minDistance = 1e-4;
-	controls.maxDistance = 1e-4;
-} else {
-	controls.maxPolarAngle = Math.PI / 2;
-	controls.minDistance = 1;
-	controls.maxDistance = 20;
-}
-
-let playerIsOnGround = false;
-let fwdPressed = false,
-bkdPressed = false,
-lftPressed = false,
-rgtPressed = false;
-let playerVelocity = new THREE.Vector3();
-let upVector = new THREE.Vector3(0, 1, 0);
-let tempVector = new THREE.Vector3();
-let tempVector2 = new THREE.Vector3();
-let tempBox = new THREE.Box3();
-let tempMat = new THREE.Matrix4();
-let tempSegment = new THREE.Line3();
-
-function updatePlayer(delta) {
-playerVelocity.y += playerIsOnGround ? 0 : delta * params.gravity;
-player.position.addScaledVector(playerVelocity, delta);
-
-// move the player
-const angle = controls.getAzimuthalAngle();
-if (fwdPressed) {
-	tempVector.set(0, 0, -1).applyAxisAngle(upVector, angle);
-	player.position.addScaledVector(tempVector, params.playerSpeed * delta);
-}
-
-if (bkdPressed) {
-	tempVector.set(0, 0, 1).applyAxisAngle(upVector, angle);
-	player.position.addScaledVector(tempVector, params.playerSpeed * delta);
-}
-
-if (lftPressed) {
-	tempVector.set(-1, 0, 0).applyAxisAngle(upVector, angle);
-	player.position.addScaledVector(tempVector, params.playerSpeed * delta);
-}
-
-if (rgtPressed) {
-	tempVector.set(1, 0, 0).applyAxisAngle(upVector, angle);
-	player.position.addScaledVector(tempVector, params.playerSpeed * delta);
-}
-
-player.updateMatrixWorld();
-
-// adjust player position based on collisions
-const capsuleInfo = player.capsuleInfo;
-tempBox.makeEmpty();
-tempMat.copy(collider.matrixWorld).invert();
-tempSegment.copy(capsuleInfo.segment);
-
-// get the position of the capsule in the local space of the collider
-tempSegment.start.applyMatrix4(player.matrixWorld).applyMatrix4(tempMat);
-tempSegment.end.applyMatrix4(player.matrixWorld).applyMatrix4(tempMat);
-
-// get the axis aligned bounding box of the capsule
-tempBox.expandByPoint(tempSegment.start);
-tempBox.expandByPoint(tempSegment.end);
-
-tempBox.min.addScalar(-capsuleInfo.radius);
-tempBox.max.addScalar(capsuleInfo.radius);
-
-collider.geometry.boundsTree.shapecast({
-	intersectsBounds: (box) => box.intersectsBox(tempBox),
-
-	intersectsTriangle: (tri) => {
-		// check if the triangle is intersecting the capsule and adjust the
-		// capsule position if it is.
-		const triPoint = tempVector;
-		const capsulePoint = tempVector2;
-
-		const distance = tri.closestPointToSegment(tempSegment, triPoint, capsulePoint);
-		if (distance < capsuleInfo.radius) {
-			const depth = capsuleInfo.radius - distance;
-			const direction = capsulePoint.sub(triPoint).normalize();
-
-			tempSegment.start.addScaledVector(direction, depth);
-			tempSegment.end.addScaledVector(direction, depth);
-		}
-	}
-});
-
-// get the adjusted position of the capsule collider in world space after checking
-// triangle collisions and moving it. capsuleInfo.segment.start is assumed to be
-// the origin of the player model.
-const newPosition = tempVector;
-newPosition.copy(tempSegment.start).applyMatrix4(collider.matrixWorld);
-
-// check how much the collider was moved
-const deltaVector = tempVector2;
-deltaVector.subVectors(newPosition, player.position);
-
-// if the player was primarily adjusted vertically we assume it's on something we should consider ground
-playerIsOnGround = deltaVector.y > Math.abs(delta * playerVelocity.y * 0.25);
-
-const offset = Math.max(0.0, deltaVector.length() - 1e-5);
-deltaVector.normalize().multiplyScalar(offset);
-
-// adjust the player model
-player.position.add(deltaVector);
-
-if (!playerIsOnGround) {
-	deltaVector.normalize();
-	playerVelocity.addScaledVector(deltaVector, -deltaVector.dot(playerVelocity));
-} else {
-	playerVelocity.set(0, 0, 0);
-}
-
-// adjust the camera
-camera.position.sub(controls.target);
-controls.target.copy(player.position);
-camera.position.add(player.position);
-
-// if the player has fallen too far below the level reset their position to the start
-if (player.position.y < -25) {
-	reset();
-}
-} -->
--- src/lib/controllers/FollowObjectController.svelte
+++ src/lib/controllers/FollowObjectController.svelte
@@ -1,35 +1,34 @@
 <script>
-	import { Vector2, Vector3 } from 'three';
+	import { Euler, Vector3 } from 'three';
 	import { DEG2RAD } from 'three/src/math/MathUtils.js';
-	import { Group, useFrame, useThrelte } from '@threlte/core';
+	import { Group, useFrame, useThrelte, PerspectiveCamera, OrbitControls } from '@threlte/core';
 	import { createEventDispatcher } from 'svelte';
 	import { Collider, CollisionGroups, RigidBody } from '@threlte/rapier';
 
 	export let position = undefined;
-	// export let camRotation;
+	export let cam
+	export let camRotation = [0, 0, 0];
 	export let height = 1.7;
 	export let radius = 0.3;
 	export let speed = 1;
 	export let jumpStrength = 3;
 	export let playerCollisionGroups = [0];
 	export let groundCollisionGroups = [15];
-
+	
 	let rigidBody;
-
-	// $: console.log(camRotation)
+	let followDistance = 10
 
 	const { renderer } = useThrelte();
 	if (!renderer) throw new Error();
 
 	const keys = {
-		up: false,
-		down: false,
-		left: false,
-		right: false
+		forward: 0,
+		backward: 0,
+		left: 0,
+		right: 0
 	};
 
 	const t = new Vector3();
-	const t2 = new Vector2();
 
 	const dispatch = createEventDispatcher();
 
@@ -38,23 +37,20 @@
 
 	useFrame(() => {
 		if (!rigidBody) return;
-		t.fromArray([0, 0, 0]);
-		if (keys.down) t.x += 1;
-		if (keys.up) t.x -= 1;
-		if (keys.left) t.z += 1;
-		if (keys.right) t.z -= 1;
-		const l = t.length();
-
-		const xzLength = t2.set(t.x, t.z).length();
-		if (l > 0) t.divideScalar(l).multiplyScalar(speed);
-
-		if (xzLength > 0) {
-			rigidBody.resetForces(true);
-			rigidBody.resetTorques(true);
-		}
+		// get direction
+		const velVec = t.fromArray([keys.left - keys.right, 0, keys.forward - keys.backward]);
+		// sort rotate and multiply by speed
+		velVec.applyEuler(new Euler().fromArray(camRotation))
+			.multiplyScalar(speed);
+		// don't override falling velocity
 		const linVel = rigidBody.linvel();
 		t.y = linVel.y;
+		// finally set the velocities and wake up the body
 		rigidBody.setLinvel(t, true);
+
+		// when body position changes update position prop for camera
+		const pos = rigidBody.translation();
+		position = {x:pos.x, y:pos.y, z:pos.z}
 	});
 
 	/**
@@ -63,16 +59,16 @@
 	function onKeyDown(e) {
 		switch (e.key) {
 			case 's':
-				keys.down = true;
+				keys.backward = 1;
 				break;
 			case 'w':
-				keys.up = true;
+				keys.forward = 1;
 				break;
 			case 'a':
-				keys.left = true;
+				keys.left = 1;
 				break;
 			case 'd':
-				keys.right = true;
+				keys.right = 1;
 				break;
 			case ' ':
 				if (!rigidBody || !grounded) break;
@@ -89,16 +85,16 @@
 	function onKeyUp(e) {
 		switch (e.key) {
 			case 's':
-				keys.down = false;
+				keys.backward = 0;
 				break;
 			case 'w':
-				keys.up = false;
+				keys.forward = 0;
 				break;
 			case 'a':
-				keys.left = false;
+				keys.left = 0;
 				break;
 			case 'd':
-				keys.right = false;
+				keys.right = 0;
 				break;
 			default:
 				break;
@@ -108,7 +104,21 @@
 
 <svelte:window on:keydown|preventDefault={onKeyDown} on:keyup|preventDefault={onKeyUp} />
 
-<RigidBody bind:rigidBody {position} enabledRotations={[false, true, false]} type={'dynamic'}>
+<PerspectiveCamera bind:camera={cam} position={{ x: 6, y: 14, z: 5 }}>
+	<OrbitControls
+		target={position}
+		minDistance={1}
+		maxDistance={10}
+		on:change={() => {
+			const camEuler = cam.rotation.clone();
+			camEuler.reorder('YXZ');
+			camEuler.x = 0;
+			camEuler.y += DEG2RAD * 180
+			camRotation = [...camEuler];
+		}} />
+</PerspectiveCamera>
+
+<RigidBody bind:rigidBody {position} enabledRotations={[false, false, false]}>
 	<CollisionGroups groups={playerCollisionGroups}>
 		<Collider shape={'capsule'} args={[height / 2 - radius, radius]} />
 	</CollisionGroups>
