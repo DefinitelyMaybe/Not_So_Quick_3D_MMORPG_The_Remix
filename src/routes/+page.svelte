f5ac05a8cb6c15d656c145b6feb0b11ef8494e16
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-08-24 09:50:23 +1200

    change dispaly name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 	<!-- <a class="btn m-1" sveltekit:prefetch href="login">Login</a> -->
 	<a class="btn m-1" href="/threlteDoor">Threlte Door</a>
 	<a class="btn m-1" href="/entityViewer">Entity Viewer</a>
-	<a class="btn m-1" href="/repl">REPL</a>
+	<a class="btn m-1" href="/repl">REPL - reactive URL</a>
 	<a class="btn m-1" href="/animation">animation</a>
 	<a class="btn m-1" href="/terrain">terrain</a>
 	<a class="btn m-1" href="/terrainV2">Character on Terrain</a>
