738fc608efdb5a1b049df2ad41975b38cac50e29
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-08-24 15:35:40 +1200

    create new scene

---
+++ src/lib/scenes/Example4.svelte
@@ -0,0 +1,44 @@
+<script>
+	import { AmbientLight, DirectionalLight, Mesh } from '@threlte/core';
+	import { AutoColliders, CollisionGroups, Debug } from '@threlte/rapier';
+	import { BoxBufferGeometry, MeshStandardMaterial } from 'three'
+	import Door from '../Door.svelte';
+	import Player from '../characters/PlayerV4.svelte';
+
+	let cam = undefined;
+
+	export const gui = {
+		getCamera() {
+			return cam;
+		}
+	};
+</script>
+
+<Debug />
+
+<DirectionalLight shadow position={{ y: 20, x: 8, z: -3 }} />
+<AmbientLight intensity={0.2} />
+
+<!--
+	The ground needs to be on both group 15 which is the group
+	to detect the groundedness of the player as well as on group
+	0 which is the group that the player is actually physically
+	interacting with.
+ -->
+<CollisionGroups groups={[0, 15]}>
+	<AutoColliders shape={'cuboid'} position={{ y: -0.5 }}>
+    <Mesh
+      receiveShadow
+      geometry={new BoxBufferGeometry(100, 1, 100)}
+      material={new MeshStandardMaterial()}
+    />
+  </AutoColliders>
+</CollisionGroups>
+
+<!--
+	All physically interactive stuff should be on group 0
+-->
+<CollisionGroups groups={[0]}>
+	<Player bind:cam position={{ x: 2 }} speed={4} />
+	<Door />
+</CollisionGroups>
