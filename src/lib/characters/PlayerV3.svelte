bfcd4717893aaa55617d743133ca76e2104136c2
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-08-29 17:23:52 +1200

    remove FirstPersonController

--- src/lib/characters/PlayerV3.svelte
+++ src/lib/characters/PlayerV3.svelte
@@ -1,134 +1,128 @@
 <script>
-	import { GLTF, useGltfAnimations, HTML } from '@threlte/extras';
-	import { modelData } from '../../data/models/mod.js';
-	import PlayerController from '../controllers/FirstPersonController.svelte';
+	import { Vector3 } from 'three';
+	import { useFrame, useThrelte, PerspectiveCamera, Group } from '@threlte/core';
+	import { RigidBody, CollisionGroups, Collider } from '@threlte/rapier';
+	import { createEventDispatcher, onDestroy } from 'svelte';
+	import PointerLockControls from '../controls/PointerLockControlsRemix.svelte';
 
 	export let position = undefined;
 	export let cam = undefined;
 
-	let scene;
-	let camRotation
+	export let playerCollisionGroups = [0];
+	export let groundCollisionGroups = [15];
+	export let radius = 0.3;
+	export let height = 1.7;
+	export let speed = 6;
+	export let jumpStrength = 4;
 
-	const { name, url } = modelData.zombie;
-	const { gltf, actions } = useGltfAnimations(({ actions }) => {
-		// socerer actions could be: Armature|mixamo.com|Layer0, Attack, Dance, Death, Idle, Run, Walk
-		// zombie actions: Attack, Death, Idle, Walk
-		actions[currentAction]?.play();
+	let rigidBody;
+	let lock;
+
+	let forward = 0
+	let backward = 0
+	let left = 0
+	let right = 0
+
+	const t = new Vector3();
+
+	const dispatch = createEventDispatcher();
+
+	let grounded = false;
+	$: grounded ? dispatch('groundenter') : dispatch('groundexit');
+
+	const lockControls = () => lock();
+	
+	const { renderer } = useThrelte();
+	if (!renderer) throw new Error();
+
+	let camRotation;
+
+	renderer.domElement.addEventListener('click', lockControls);
+
+	onDestroy(() => {
+		renderer.domElement.removeEventListener('click', lockControls);
 	});
-	let currentAction = 'Idle';
-	let movementKeysDown = 0;
-	let forward = false;
-	let backward = false;
-	let left = false;
-	let right = false;
 
-	$: if (camRotation && scene) {
-		scene.rotation.set(camRotation[0], camRotation[1], camRotation[2]);
-	}
+	useFrame(() => {
+		if (!rigidBody) return;
+		// get direction
+		const velVec = t.fromArray([right - left, 0, backward - forward]);
+		// sort rotate and multiply by speed
+		velVec.applyEuler(cam.rotation).multiplyScalar(speed);
+		// don't override falling velocity
+		const linVel = rigidBody.linvel();
+		t.y = linVel.y;
+		// finally set the velocities and wake up the body
+		rigidBody.setLinvel(t, true);
 
-	function transitionTo(newAction, duration = 1) {
-		const action = $actions[currentAction];
-		const nextAction = $actions[newAction];
-		if (action === nextAction) return;
-		// Function inspired by: https://github.com/mrdoob/three.js/blob/master/examples/webgl_animation_skinning_blending.html
-		nextAction.enabled = true;
+		// when body position changes update position prop for camera
+		const pos = rigidBody.translation();
+		position = { x: pos.x, y: pos.y, z: pos.z };
+	});
 
-		if (action) {
-			action.crossFadeTo(nextAction, duration, true);
-		}
-
-		// Not sure why I need this but the source code does not
-		nextAction.play();
-		currentAction = newAction;
-	}
-
-	function handleKeyDown(e) {
+	/** @param {KeyboardEvent} e */
+	function onKeyDown(e) {
 		switch (e.key) {
+			case 's':
+				backward = 1;
+				break;
 			case 'w':
-				if (movementKeysDown == 0) {
-					transitionTo('Walk', 0.2);
-				}
-				if (!forward) {
-					movementKeysDown += 1;
-					forward = true;
-				}
+				forward = 1;
 				break;
 			case 'a':
-				if (movementKeysDown == 0) {
-					transitionTo('Walk', 0.2);
-				}
-				if (!left) {
-					movementKeysDown += 1;
-					left = true;
-				}
+				left = 1;
 				break;
 			case 'd':
-				if (movementKeysDown == 0) {
-					transitionTo('Walk', 0.2);
-				}
-				if (!right) {
-					movementKeysDown += 1;
-					right = true;
-				}
+				right = 1;
 				break;
-			case 's':
-				if (movementKeysDown == 0) {
-					transitionTo('Walk', 0.2);
-				}
-				if (!backward) {
-					movementKeysDown += 1;
-					backward = true;
-				}
+			case ' ':
+				if (!rigidBody || !grounded) break;
+				rigidBody.applyImpulse({ x: 0, y: jumpStrength, z: 0 }, true);
 				break;
 			default:
 				break;
 		}
 	}
 
-	function handleKeyUp(e) {
+	/** @param {KeyboardEvent} e */
+	function onKeyUp(e) {
 		switch (e.key) {
-			case 'w':
-				forward = false;
-				movementKeysDown -= 1;
-				break;
 			case 's':
-				backward = false;
-				movementKeysDown -= 1;
+				backward = 0;
+				break;
+			case 'w':
+				forward = 0;
 				break;
 			case 'a':
-				left = false;
-				movementKeysDown -= 1;
+				left = 0;
 				break;
 			case 'd':
-				right = false;
-				movementKeysDown -= 1;
+				right = 0;
 				break;
 			default:
 				break;
-		}
-
-		if (movementKeysDown == 0) {
-			transitionTo('Idle', 0.2);
 		}
 	}
 </script>
 
-<svelte:window on:keydown={handleKeyDown} on:keyup={handleKeyUp} />
+<svelte:window on:keydown={onKeyDown} on:keyup={onKeyUp} />
 
-<!-- To detect the groundedness of the player, a collider on group 15 is used -->
-<PlayerController
-	{position}
-	speed={6}
-	jumpStrength={4}
-	bind:cam
-	bind:camRotation>
-	<GLTF {url} receiveShadow castShadow bind:scene bind:gltf={$gltf}>
-		<HTML center transform distanceFactor={2} position={{ y: 2.2 }} lookAt={cam}>
-			<button
-				class="btn"
-				on:click={() => {
-					console.log(`${name} clicked`);
-				}}>{name}</button>
-		</HTML>
-	</GLTF>
-</PlayerController>
+<PerspectiveCamera bind:camera={cam} bind:position fov={90}>
+	<PointerLockControls bind:lock />
+</PerspectiveCamera>
+
+<RigidBody bind:rigidBody {position} enabledRotations={[false, false, false]}>
+	<CollisionGroups groups={playerCollisionGroups}>
+		<Collider shape={'capsule'} args={[height / 2 - radius, radius]} />
+	</CollisionGroups>
+
+	<CollisionGroups groups={groundCollisionGroups}>
+		<Collider
+			sensor
+			on:sensorenter={() => (grounded = true)}
+			on:sensorexit={() => (grounded = false)}
+			shape={'ball'}
+			args={[radius * 1.2]}
+			position={{ y: -height / 2 + radius }} />
+	</CollisionGroups>
+</RigidBody>
