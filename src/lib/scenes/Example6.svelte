60c5fcf331d51121ea5cd38e025b3c15f93fdb4b
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-08-31 16:40:44 +1200

    player has collision groups already + fmt

--- src/lib/scenes/Example6.svelte
+++ src/lib/scenes/Example6.svelte
@@ -3,8 +3,8 @@
 	import { CollisionGroups, Debug } from '@threlte/rapier';
 	import Door from '../Door.svelte';
 	import Player from '../characters/PlayerV4.svelte';
-  import Terrain from "../TerrainV2.svelte";
-  import Background from "../SpaceBackground.svelte";
+	import Terrain from '../TerrainV2.svelte';
+	import Background from '../SpaceBackground.svelte';
 
 	let cam = undefined;
 
@@ -18,12 +18,13 @@
 <Debug />
 
 <DirectionalLight shadow position={{ y: 20, x: 8, z: -3 }} />
-<HemisphereLight groundColor={'#424a75'} skyColor={'#6a88b5'} intensity={0.7}/>
+<HemisphereLight groundColor={'#424a75'} skyColor={'#6a88b5'} intensity={0.7} />
 <AmbientLight intensity={0.2} />
 
 <Background />
 <FogExp2 color={'#89b2eb'} density={0.01} />
 
+<Player bind:cam position={{ x: 2, y: 15 }} />
 <!--
 	The ground needs to be on both group 15 which is the group
 	to detect the groundedness of the player as well as on group
@@ -31,13 +32,15 @@
 	interacting with.
  -->
 <CollisionGroups groups={[0, 15]}>
-	<Terrain />
+	<Terrain
+		on:ready={() => {
+			console.log('hello world');
+		}} />
 </CollisionGroups>
 
 <!--
 	All physically interactive stuff should be on group 0
 -->
 <CollisionGroups groups={[0]}>
-	<Player bind:cam position={{ x: 2, y: 10 }} />
-	<Door position={{y:4}}/>
+	<Door position={{ y: 4 }} />
 </CollisionGroups>
